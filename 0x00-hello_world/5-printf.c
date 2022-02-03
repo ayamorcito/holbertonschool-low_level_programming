6c8e66c05135217a88da3cda01bba451cb6aa59d
ayamorcito <izquierdoz.g@gmail.com>
2022-02-03 10:15:28 -0800

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the specified phrase
+ * 
+ * Return: 0 allways if correct, other if not
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
