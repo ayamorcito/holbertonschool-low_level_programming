c96fa453b9650275b08a1bca94c719afd385b7d2
ayamorcito <izquierdoz.g@gmail.com>
2022-02-03 09:39:45 -0800

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints in the S-O the specified phrase  
+ * Return: 0 allways if correct, other if not 
+ * 
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
