c4dd9f54432d16f35eca3ff2883922f7102ecff6
ayamorcito <izquierdoz.g@gmail.com>
2022-02-04 10:09:29 -0800

    0final

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - prints the size of specfied
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
 {
 	int n;
