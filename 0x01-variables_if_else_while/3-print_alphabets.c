9659d66d3fc0f19cb48bb35779da924223821364
ayamorcito <izquierdoz.g@gmail.com>
2022-02-04 11:47:00 -0800

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lower then upper case
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		{
+		putchar(ch);
+		}	
+	       	for (ch = 'A'; ch <= 'Z'; ch++)
+                {
+                putchar(ch);
+
+			}
+	putchar('\n');
+return (0);
+}
