d4b8550a5633f720757fa0959780a81e39954cbd
ayamorcito <izquierdoz.g@gmail.com>
2022-02-07 09:35:40 -0800

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("0123456789");
+
+	return (0);
+}
