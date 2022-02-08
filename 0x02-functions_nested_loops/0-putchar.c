60d66edb8fb1cf93aaf66719858351e353617153
ayamorcito <izquierdoz.g@gmail.com>
2022-02-08 09:14:26 -0800

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/* main - prints _putchar and a new line
+ * return: allways 0 if corect, other if not
+*/
+
+int main (void) {
+	int a;
+	char nombre[] = "_putchar";
+
+	for (a = 0 ; a < 8 ; a++)
+	{
+		_putchar(nombre[a]);
+	}
+	_putchar('\n');
+	return (0);
+}
