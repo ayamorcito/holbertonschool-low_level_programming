7da73f53e1dc4de72d1f12f7e19748743c6301a3
ayamorcito <izquierdoz.g@gmail.com>
2022-02-16 08:47:05 -0800

    2full

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,18 +5,17 @@
  * @dest: destination string
  * @src: source string
  * @n: number of bytes
- * 
  * Return: a pointer to dest
  */
 
-char *_strncpy(char *dest, const char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
-    int i;
+	int i;
 
-   for (i = 0; i < n && src[i] != '\0'; i++)
-        dest[i] = src[i];
-    for ( ; i < n; i++)
-        dest[i] = '\0';
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	dest[i] = src[i];
+	for ( ; i < n; i++)
+		dest[i] = '\0';
 
-   return dest;
+	return (dest);
 }
