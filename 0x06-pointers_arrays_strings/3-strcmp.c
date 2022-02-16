669740a6c32c068c8213a13d451e3399208461c8
ayamorcito <izquierdoz.g@gmail.com>
2022-02-16 09:09:17 -0800

    3full

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncmy - compares two strings
+ * @s1: 1st string
+ * @s2: 2d string
+ *  
+ * Return: a pointer to dest
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
