3344ca6883b8852e21430ba507ce80454cc08a2a
ayamorcito <izquierdoz.g@gmail.com>
2022-02-23 09:40:32 -0800

    1-corrected

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * void _print_rev_recursion - function that prints a string in reverse
+ * _print_rev_recursion - function that prints a string in reverse
  * @s: string to be printed
  */
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
 		return;
 	}
 	else
