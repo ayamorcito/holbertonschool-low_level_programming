b825f4940c920222c66ab182cc53ccf4883a67b7
ayamorcito <izquierdoz.g@gmail.com>
2022-02-25 09:16:00 -0800

    2full

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *main - prints the arguments it receives
+ *@argc: number of arguments used
+ *@argv: array of pointers (arguments)
+ *Return: 0 if correct
+ */
+
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+
+	for(i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
