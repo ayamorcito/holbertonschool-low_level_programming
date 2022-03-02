8480ca7fed1b8ae173efbfd06462fcadfe958529
ayamorcito <izquierdoz.g@gmail.com>
2022-03-02 07:52:27 -0800

    4corrected

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,39 +1,41 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 #include <ctype.h>
-#include <string.h>
+
 /**
- *main - adds positive numbers
- *@argc: number of arguments used
- *@argv: array of pointers (arguments)
- *Return: 0 if correct, 1 if no arguments used
+ * main - multiplies two numbers passed by the args
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: 0 Success, 1 error (args not numbers)
  */
-
-
 int main(int argc, char *argv[])
 {
-	unsigned int i;
-	int j, add = 0;
+	int sum = 0, i, j;
+	char *str;
 
-	if (argc == 1)
+	if (argc > 1)
 	{
-		printf("0\n");
+		for (j = 1; j < argc; j++)
+		{
+			str = argv[j];
+
+			for (i = 0; str[i] != '\0'; i++)
+			{
+				if (isdigit(str[i]) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
+		sum += atoi(argv[j]);
+		}
+
+		printf("%i\n", sum);
 		return (0);
 	}
 
-	for (j = 1; j < argc; j++)
-	{
-		for (i = 0; i < strlen(argv[j]); i++)
-		{
-			if (!isdigit(argv[i][j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		add += atoi(argv[j]);
-	}
-	printf("%d\n", add);
+	printf("0\n");
 	return (0);
 }
