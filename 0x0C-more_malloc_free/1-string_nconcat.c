bf5876eccd84472fd6ac5f288155e91a76b7c451
ayamorcito <izquierdoz.g@gmail.com>
2022-03-03 12:11:43 -0800

    1corr

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,7 +11,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
-	unsigned int i = 0, st1 = 0, st2 = 0;
+	unsigned int i = 0, j = 0, st1 = 0, st2 = 0;
 	if (s1 == NULL)
 	{
 		s1 = "";
@@ -41,9 +41,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			{
 				p[i] = s1[i];
 			}
-			for ( ; i < st1 + (n + 1); i++)
+			for ( ; i < st1 + n; i++, j++)
 			{
-				p[i] = s2[i];
+				p[i] = s2[j];
 			}
 			p[i] = '\0';
 			return (p);
