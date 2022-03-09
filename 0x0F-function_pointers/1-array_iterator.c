250d424684e577a08091eb7e8e59111fce657769
ayamorcito <izquierdoz.g@gmail.com>
2022-03-09 09:08:58 -0800

    1corr

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,11 +11,11 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i = 0;
+	unsigned int i;
 
 	if (!size || !action)
 		return;
 
-	for ( ; i < size; i++)
+	for (i = 0; i < size; i++)
 		action(array[i]);
 }
