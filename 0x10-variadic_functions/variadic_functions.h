b66c1b162d33fcdf1e6e0d751aaa17f42b863e21
ayamorcito <izquierdoz.g@gmail.com>
2022-03-11 17:48:24 -0800

    3corr

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -10,8 +10,9 @@
 typedef struct chfon
 {
 	char *x;
-	void (*f)(va_list p)
+	void (*f)(va_list p);
 } chfun;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
