f801fd64b0817f4a1d28a774256112d2ecc78e56
ayamorcito <izquierdoz.g@gmail.com>
2022-03-23 12:42:15 -0700

    6corr

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -2,24 +2,24 @@
 #include "lists.h"
 
 /**
- * pop_listin - deletes the head node of a listint_t linked list, and returns the head node’s data
+ * pop_listint - deletes the head of linked list, returns the node’s data
  * @head: first node of the list_t list to be freed
- */
+ * Return: int value in head
+ **/
 int pop_listint(listint_t **head)
 {
-	int temp;
-	
+	int val;
+	listint_t *temp;
+
 	if (*head == NULL)
-		return(0); 
-	
+		return (0);
+
 	if (*head != NULL)
 	{
-		temp = (*head)->n;
-		*head = (*head)->next;
+		temp = *head;
+		val = (*head)->n;
+		*head = (temp)->next;
+		free(temp);
 	}
-	if ((*head)->next == NULL)
-	{
-		free(*head);
-	}
-	return(temp);
+	return (val);
 }
