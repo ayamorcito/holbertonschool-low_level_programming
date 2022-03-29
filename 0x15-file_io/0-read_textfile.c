de2d102647c60441f7621f4e3ce1dc168e94896e
ayamorcito <izquierdoz.g@gmail.com>
2022-03-29 10:42:06 -0700

    0 full + readme + others

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * read_textfile -  reads a text file and prints it to the POSIX stdout
+ * @filename: the filename
+ * @letters: number of letter it should read and print
+ *
+ **/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd = 0;
+	char *buff = malloc(sizeof(char) * letters);
+	int count = 0, wr = 0;
+
+	fd = open(filename, O_RDONLY);
+
+	if (!filename || filename == NULL)
+		return (0);
+
+	if (buff == NULL)
+		return (0);
+	if (fd == -1)
+		return (0);
+
+	count = read(fd, buff, letters);
+
+	wr = write(STDOUT_FILENO, buff, count);
+
+	if (count == -1 || wr == -1 || fd == -1 || wr != count)
+	{
+		return (0);
+	}
+
+	free(buff);
+	close(fd);
+
+	return (wr);
+
+
+
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+In this folder i will upload the tasks for the 0x15-file_io objective
