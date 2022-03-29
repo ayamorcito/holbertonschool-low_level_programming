7ebc44d4e29d522af6f7012774d59c10595fa935
ayamorcito <izquierdoz.g@gmail.com>
2022-03-29 11:48:27 -0700

    1corr

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd = 0, i = 0, wr = 0;
 
-	fd = open(filename, O_CREAT |O_RDWR | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (filename == NULL)
 		return (-1);
