0c6c2fc7d490cee1060adabe430946adfa3eccf9
ayamorcito <izquierdoz.g@gmail.com>
2022-05-05 10:49:23 -0700

    1full+readme

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,31 @@
+#ifndef AMORCITO
+#define AMORCITO
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+ /**
+ * struct dlistint_s - doubly linked list
+ * @n: int
+ * @prev: points to the prev node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * 
+ */
+
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+
+
+
+
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+
+
+#endif
