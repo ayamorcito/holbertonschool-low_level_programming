ffb98530d2695bf89bd1dc3f89ccd03642517c1f
ayamorcito <izquierdoz.g@gmail.com>
2022-08-18 10:58:21 -0700

    2 style fix

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,8 +1,9 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - implementation of the djb2 algorithm
- * @str: string used to generate hash value
+ * key_index - function that gives you the index of a key
+ * @key: is the key
+ * @size: is the size of the array of the hash table
  *
  * Return: hash value
  */
@@ -13,7 +14,5 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 	unsigned long int index = 0;
 
 	index = (hash_djb2(key) % size);
-	
-	return (index);	
-
+	return (index);
 }
