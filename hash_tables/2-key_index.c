23d84d78d7780f4bb4deee7823d1a505c5a518dd
ayamorcito <izquierdoz.g@gmail.com>
2022-08-18 10:53:17 -0700

    2 full

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+
+	unsigned long int index = 0;
+
+	index = (hash_djb2(key) % size);
+	
+	return (index);	
+
+}
