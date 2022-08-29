0b521a177e244df43442030ab12b5b99c24f58c4
ayamorcito <izquierdoz.g@gmail.com>
2022-08-29 10:37:58 -0700

    coding style fix

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -5,20 +5,20 @@
  * @array: is a pinter to the first element of the array to search in
  * @size: is the number of the elements in array
  * @value: is the value to search for
+ * Return: the first index where value is located
  *
- **/ 
+ **/
 
 int linear_search(int *array, size_t size, int value)
 {
 	 int i = 0;
 	 int sz = size;
-	 
-	 for (i = 0; i < sz; i++)
-	 {
-		 printf("Value checked array[%d] = [%d]\n", i, array[i]);
-		 
-		 if (array[i] == value)
-			 return (i);
-	 }
-	 return (-1);
+
+	for (i = 0; i < sz; i++)
+	{
+		printf("Value checked array[%d] = [%d]\in", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
