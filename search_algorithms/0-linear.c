fb036686f155acc8b8ac743384fdcb23c5170778
ayamorcito <izquierdoz.g@gmail.com>
2022-08-29 10:29:29 -0700

    0 full

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches 4 value in an array of int using Linearsearch algo
+ * @array: is a pinter to the first element of the array to search in
+ * @size: is the number of the elements in array
+ * @value: is the value to search for
+ *
+ **/ 
+
+int linear_search(int *array, size_t size, int value)
+{
+	 int i = 0;
+	 int sz = size;
+	 
+	 for (i = 0; i < sz; i++)
+	 {
+		 printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		 
+		 if (array[i] == value)
+			 return (i);
+	 }
+	 return (-1);
+}
---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef AMORCITO
+#define AMORCITO
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+
+#endif
